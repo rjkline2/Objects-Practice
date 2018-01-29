8b9c9e57dc7b98bdb04b6a9c5e8da88fef8e5df8
RJ Kline <rj.kline@gmail.com>
2018-01-29 11:40:43 -0500

    Updated Projcet

--- Cat.cs
+++ Cat.cs
@@ -12,13 +12,37 @@ namespace Objects_Practice
         
             //fields
         private string name;
+        private int age;
+        private string furColor;
+        private bool isHungry = true;
 
+
+        // Properties
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
        // I need BEHAVIORS
         // Contructors
        public Cat()
         {
             //default contructor
-            //takes no parameters (nothing in the parentheses)
+            //takes no parameters (nothing in the parentheses, ever.)
+        }
+        public Cat(string name, int age, string furColor)
+        {
+            this.name = name;
+            this.age = age;
+            this.furColor = furColor;
+        }
+        public void Eat()
+        {
+            if (isHungry)
+            {
+                isHungry = false;
+            }
+            Console.WriteLine("Is the cat hungry?" + isHungry );
         }
     }
 }
